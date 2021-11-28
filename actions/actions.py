1bd653c12376a1f6ca4594415b3b0a0cbfa219c8
Paul-Jason Mello <mello.pauljason@gmail.com>
2021-11-28 03:11:45 -0800

    Updating User Query

--- actions/actions.py
+++ actions/actions.py
@@ -8,6 +8,7 @@
 # This is a simple example for a custom action which utters "Hello World!"
 
 import json
+import pandas as pd
 from alpha_vantage.timeseries import TimeSeries
 from pathlib import Path
 from typing import Any, Text, Dict, List
@@ -17,32 +18,156 @@ from rasa_sdk.knowledge_base.storage import InMemoryKnowledgeBase
 from rasa_sdk.knowledge_base.actions import ActionQueryKnowledgeBase
 
 #knowledge = Path("data/TickerNames.txt").read_text().split("\n")
-#tic = "YJH452CREZ8Z0DC0"
 
 #format = TimeSeries(key=tic, output_format='pandas')
 #data = format.get <SOME TIME FRAME>('<TICKER SYMBOL>')
 
 #data[0]
 
-class ActionCheckTickerExistence(Action):
+tic = "YJH452CREZ8Z0DC0"
+knowledge = Path("data/TickerNames.txt").read_text().split("\n")
 
+class ActionCheckTicker(Action):    # Used to ensure ticker is valid for remaining work
     def name(self) -> Text:
-        return "action_check_ticker_existence"
-
+        return "action_check_valid_ticker"
     def run(self, dispatcher: CollectingDispatcher,
             tracker: Tracker,
             domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
-        knowledge = Path("data/TickerNames.txt").read_text().split("\n")
         for msg in tracker.latest_message['entities']:
             print(tracker.latest_message)
-            tic_name = msg['value'].upper()
-            if tic_name in knowledge:
-                dispatcher.utter_message(template="utter_valid_ticker_true", name= tic_name)
-                return []
+            name = msg['value'].upper()
+            if name in knowledge:
+                return True
             else:
-                dispatcher.utter_message(template="utter_valid_ticker_false", name= tic_name)
-                return []
-        return []
+                return False
+
+CheckTickerValidity = ActionCheckTicker()
+
+class ActionTickerExistence(Action):    # Used for user to find/ensure ticker is valid
+    def name(self) -> Text:
+        return "action_ticker_existence"
+    def run(self, dispatcher: CollectingDispatcher,
+            tracker: Tracker,
+            domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+        for msg in tracker.latest_message['entities']:
+            print(tracker.latest_message)
+            name = msg['value'].upper()
+        if name in knowledge:
+            dispatcher.utter_message(template="utter_valid_ticker_true", name = name)
+            return []
+        else:
+            dispatcher.utter_message(template="utter_valid_ticker_false", name = name)
+            return []
+
+# Below we account for checking and returning todays High
+# -------------------------------------------------------
+class ActionCheckHigh(Action):
+    def name(self) -> Text:
+        return "action_check_todays_high"
+    def run(self, dispatcher: CollectingDispatcher,
+            tracker: Tracker,
+            domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+        for msg in tracker.latest_message['entities']:
+            print(tracker.latest_message)
+            name = msg['value'].upper()
+        if (CheckTickerValidity.run(dispatcher, tracker, domain)):
+            highVal = get_ticker_info_adjusted_daily(name, '2. high')
+            dispatcher.utter_message(text= f"Todays high for {name} was {highVal}.")
+            return []
+        else:
+            dispatcher.utter_message(template="utter_unfound_ticker_symbol", name = name)
+            return []
+# ------------------------------------------------------
+
+# Below we account for checking and returning todays Low
+# ------------------------------------------------------
+class ActionCheckLow(Action):
+    def name(self) -> Text:
+        return "action_check_todays_low"
+    def run(self, dispatcher: CollectingDispatcher,
+            tracker: Tracker,
+            domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+        for msg in tracker.latest_message['entities']:
+            print(tracker.latest_message)
+            name = msg['value'].upper()
+        if (CheckTickerValidity.run(dispatcher, tracker, domain)):
+            lowVal = get_ticker_info_adjusted_daily(name, '3. low')
+            dispatcher.utter_message(text= f"Todays low for {name} was {lowVal}.")
+            return []
+        else:
+            dispatcher.utter_message(template="utter_unfound_ticker_symbol", name = name)
+            return []
+# --------------------------------------------------------
+
+# Below we account for checking and returning todays Open
+# --------------------------------------------------------
+class ActionCheckOpen(Action):
+    def name(self) -> Text:
+        return "action_check_todays_open"
+    def run(self, dispatcher: CollectingDispatcher,
+            tracker: Tracker,
+            domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+        for msg in tracker.latest_message['entities']:
+            print(tracker.latest_message)
+            name = msg['value'].upper()
+        if (CheckTickerValidity.run(dispatcher, tracker, domain)):
+            val = get_ticker_info_adjusted_daily(name, '1. open')
+            dispatcher.utter_message(text= f"Today {name} opened at {val}.")
+            return []
+        else:
+            dispatcher.utter_message(template="utter_unfound_ticker_symbol", name = name)
+            return []
+# --------------------------------------------------------
+
+# Below we account for checking and returning todays Close
+# --------------------------------------------------------
+class ActionCheckClose(Action):
+    def name(self) -> Text:
+        return "action_check_todays_close"
+    def run(self, dispatcher: CollectingDispatcher,
+            tracker: Tracker,
+            domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+        for msg in tracker.latest_message['entities']:
+            print(tracker.latest_message)
+            name = msg['value'].upper()
+        if (CheckTickerValidity.run(dispatcher, tracker, domain)):
+            val = get_ticker_info_adjusted_daily(name, '4. close')
+            dispatcher.utter_message(text= f"Today {name} closed at {val}.")
+            return []
+        else:
+            dispatcher.utter_message(template="utter_unfound_ticker_symbol", name = name)
+            return []
+# ------------------------------------------------------------------
+
+# Below we account for checking and returning todays trading Volume
+# ------------------------------------------------------------------
+class ActionCheckVolume(Action):
+    def name(self) -> Text:
+        return "action_check_todays_volume"
+    def run(self, dispatcher: CollectingDispatcher,
+            tracker: Tracker,
+            domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+        for msg in tracker.latest_message['entities']:
+            print(tracker.latest_message)
+            name = msg['value'].upper()
+        if (CheckTickerValidity.run(dispatcher, tracker, domain)):
+            val = get_ticker_info_adjusted_daily(name, '6. volume')
+            dispatcher.utter_message(text= f"Todays traded volume for {name} was {val}.")
+            return []
+        else:
+            dispatcher.utter_message(template="utter_unfound_ticker_symbol", name = name)
+            return []
+# ------------------------------------------------------
+
+# Inefficent API call, in the future we could save a large chunk of data and pull from local storage instead
+# For now this suits our needs because it gives us the ability to make 500 free api calls a day
+def get_ticker_info_adjusted_daily(ticker, request):
+    TS = TimeSeries(key='YJH452CREZ8Z0DC0', output_format='pandas')
+    data = TS.get_daily_adjusted(ticker, outputsize='compact')
+    data = data[0]
+    data = pd.DataFrame(data)
+    val = data[request][0]
+    return val
 
 class KnowledgeBase(ActionQueryKnowledgeBase):
     def __init__(self):
